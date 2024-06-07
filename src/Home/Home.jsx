c87eee31b230200643106f6da2f795f76c0003a4
ziamnoun <ziamziam05@gmail.com>
2024-06-07 15:42:36 +0600

    BAnner done

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -3,7 +3,16 @@ import React from 'react';
 
 const Home = () => {
     return (
-      <div className="div">jjd</div>
+      <div className="hero min-h-screen bg-transparent">
+  <div className="hero-content text-center text-white">
+    <div className="max-w-md mx-auto">
+      <h1 className="text-5xl font-bold mb-4">Achieve Your Fitness Goals</h1>
+      <p className="mb-8">Transform your body, mind, and life with our personalized fitness programs and expert guidance.</p>
+      <button className="btn bg-[#333333] text-white">Get Started</button>
+    </div>
+  </div>
+</div>
+
     );
 };
 
