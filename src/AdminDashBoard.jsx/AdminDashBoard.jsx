db52545b602d9be71c0abd44f2013fa77026cd5d
ziamnoun <ziamziam05@gmail.com>
2024-06-10 14:07:15 +0600

    Get all users

--- src/AdminDashBoard.jsx/AdminDashBoard.jsx
+++ src/AdminDashBoard.jsx/AdminDashBoard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { HiChartPie, HiViewBoards, HiInbox, HiUser, HiShoppingBag, HiArrowSmRight, HiTable } from 'react-icons/hi';
+import { Link } from 'react-router-dom';
 
 const AdminDashBoard = () => {
     return (
@@ -14,9 +15,9 @@ const AdminDashBoard = () => {
           <li className="p-4 flex items-center hover:bg-gray-700">
             <HiInbox className="mr-3" /> Inbox
           </li>
-          <li className="p-4 flex items-center hover:bg-gray-700">
-            <HiUser className="mr-3" /> Users
-          </li>
+          <Link to='/AllUsers'><li className="p-4 flex items-center hover:bg-gray-700">
+            <HiUser className="mr-3" />Users
+          </li></Link>
           <li className="p-4 flex items-center hover:bg-gray-700">
             <HiShoppingBag className="mr-3" /> Products
           </li>
